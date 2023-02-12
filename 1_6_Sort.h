98e69f141a511dba15f7a7909eb07c3b78d797fb
Sqyqyqy <108405232+Sqyqyqy@users.noreply.github.com>
2023-02-12 23:46:03 +0800

    1_6 Sort.h

---
+++ 1_6_Sort.h
@@ -0,0 +1,11 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<string.h>
+
+void InsertSort(int* a, int n);
+void ShellSort(int* a, int n);
+void PrintArray(int* a, int n);
