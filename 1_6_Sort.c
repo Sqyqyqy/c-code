08cdc139c3e676b9b3a0b40fa98558f568ac8e05
Sqyqyqy <108405232+Sqyqyqy@users.noreply.github.com>
2023-02-20 22:56:13 +0800

    1_6 Sort.c

--- 1_6_Sort.c
+++ 1_6_Sort.c
@@ -90,3 +90,170 @@ void ShellSort(int* a, int n)
 		//PrintArray(a, n);//观察每趟排序的效果
 	}
 }
+
+
+void Swap(int* x, int* y)
+{
+	int tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
+
+
+void SelectSort(int* a, int n)
+{
+	int begin = 0, end = n - 1;
+
+	while (begin < end)
+	{
+		int maxi = begin, mini = begin;
+		for (int i = begin + 1; i <= end; i++)
+		{
+			if (a[i] > a[maxi])
+			{
+				maxi = i;
+			}
+			if (a[i] < a[mini])
+			{
+				mini = i;
+			}
+		}
+		Swap(&a[mini], &a[begin]);
+		if (maxi == begin)
+		{
+			maxi = mini;
+		}
+		Swap(&a[maxi], &a[end]);
+		begin++;
+		end--;
+	}
+}
+
+
+void BubbleSort(int* a, int n)
+{
+	for (int i = 0; i < n - 1; i++)
+	{
+		int flag = 0;
+		for (int j = 0; j < n - 1 - i; j++)
+		{
+			if (a[j] > a[j + 1])
+			{
+				Swap(&a[j], &a[j + 1]);
+				flag = 1;
+			}
+		}
+		if (flag == 0)
+			break;
+	}
+}
+
+
+//三数取中
+int GetMidIndex(int* a, int begin, int end)
+{
+	int mid = (begin + end) / 2;
+	if (a[begin] > a[mid])
+	{
+		if (a[end] > a[mid])
+		{
+			if (a[begin] > a[end])
+			{
+				return end;
+			}
+			else
+			{
+				return begin;
+			}
+		}
+		else
+		{
+			return mid;
+		}
+	}
+	else
+	{
+		if (a[begin] > a[end])
+		{
+			return begin;
+		}
+		else
+		{
+			if (a[mid] > a[end])
+			{
+				return end;
+			}
+			else
+			{
+				return mid;
+			}
+		}
+	}
+}
+
+//void QuickSort(int* a, int begin,int end)
+//{
+//	if (begin >= end)
+//		return;
+//
+//	//小区间优化
+//	if ((end - begin + 1) <= 15)
+//	{
+//		//小区间用直接插入排序处理，优化掉百分之八九十的递归调用
+//		InsertSort(a + begin, end - begin + 1);
+//	}
+//	else
+//	{
+//		//三数取中优化
+//		int mid = GetMidIndex(a, begin, end);
+//		Swap(&a[mid], &a[begin]);
+//
+//		int left = begin, right = end;
+//		int keyi = left;
+//		while (left < right)
+//		{
+//			//左边先走 找小
+//			while (left < right && a[right] >= a[keyi])
+//			{
+//				right--;
+//			}
+//			//右边再走 找大
+//			while (left < right && a[left] <= a[keyi])
+//			{
+//				left++;
+//			}
+//			Swap(&a[left], &a[right]);
+//		}
+//		Swap(&a[keyi], &a[left]);
+//
+//		QuickSort(a, begin, left - 1);
+//		QuickSort(a, left + 1, end);
+//	}
+//}
+
+//Hoare
+int PartSort1(int* a, int begin, int end)
+{
+	//三数取中优化
+	int mid = GetMidIndex(a, begin, end);
+	Swap(&a[mid], &a[begin]);
+
+	int left = begin, right = end;
+	int keyi = left;
+	while (left < right)
+	{
+		//左边先走 找小
+		while (left < right && a[right] >= a[keyi])
+		{
+			right--;
+		}
+		//右边再走 找大
+		while (left < right && a[left] <= a[keyi])
+		{
+			left++;
+		}
+		Swap(&a[left], &a[right]);
+	}
+	Swap(&a[keyi], &a[left]);
+	return left;
+}
